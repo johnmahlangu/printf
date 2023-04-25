58116b2d58cf1ab3b8ec325695342cce0ac46d16
yidna <ydtamire16@gmail.com>
2023-04-25 03:53:56 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -11,65 +11,65 @@
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	int i;
-	va_list args;
-	char *s;
-	int cnt;
-	char c;
-	int j;
-	int d;
+int len = 0;
+int i;
+va_list args;
+char *s;
+int cnt;
+char c;
+int j;
+int d;
 	
-	if (format == NULL)
-		return (-1);
+if (format == NULL)
+return (-1);
 
-	va_start(args, format);
+va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		c = format[i];
+for (i = 0; format[i] != '\0'; i++)
+{
+c = format[i];
 
-		if (c == '%')
-		{
-			i++;
-			c = format[i];
+if (c == '%')
+{
+i++;
+c = format[i];
 
-			switch (c)
-			{
-				case 's':
-					s = va_arg(args, char *);
-					for (cnt = 0; s[cnt] != '\0'; cnt++);
-					len += cnt;
-					printf("%s", s);
-					break;
-				case 'c':
-					c = va_arg(args, int);
-					len++;
-					printf("%c", c);
-					break;
-				case 'd':
-					d = va_arg(args, int);
-					len += printf("%d", d);
-					break;
-				case 'i':
-					j = va_arg(args, int);
-					len += printf("%i", j);
-					break;
-				case '%':
-					printf("%c", '%');
-					len++;
-					break;
-				default:
-					printf("%c", c);
-					len++;
-			}
-		}
-		else
-		{
-			printf("%c", format[i]);
-			len++;
-		}
-	}
-	va_end(args);
-	return (len);
+switch (c)
+{
+case 's':
+s = va_arg(args, char *);
+for (cnt = 0; s[cnt] != '\0'; cnt++);
+len += cnt;
+return(write(1, format[i], 1));
+break;
+case 'c':
+c = va_arg(args, int);
+len++;
+printf("%c", c);
+break;
+case 'd':
+d = va_arg(args, int);
+len += printf("%d", d);
+break;
+case 'i':
+j = va_arg(args, int);
+len += printf("%i", j);
+break;
+case '%':
+printf("%c", '%');
+len++;
+break;
+default:
+printf("%c", c);
+len++;
+}
+}
+else
+{
+printf("%c", format[i]);
+len++;
+}
+}
+va_end(args);
+return (len);
 }
